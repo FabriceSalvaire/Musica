0ff07fec5094575053a422bd3c306ddaf7fde9d8
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-10-24 20:02:19 +0200

    Improved MusicTheory.py

--- Musica/Math/MusicTheory.py
+++ Musica/Math/MusicTheory.py
@@ -18,7 +18,9 @@
 #
 ####################################################################################################
 
-"""String Vibration
+"""
+
+String Vibration
 ----------------
 
 There are four physical quantities involved in the string vibration phenomenon :
@@ -353,8 +355,109 @@ class PythagoreanTuningSingleton:
     def wolf_interval(self):
         return self._wolf_interval
 
+####################################################################################################
 
 PythagoreanTuning = PythagoreanTuningSingleton()
+
+####################################################################################################
+
+class AdditionModuloGroup:
+
+    """Group of Addition Modulo a Positive Integer
+
+    Group Definition
+
+    A group is a set, G, together with an operation • (called the group law of G) that combines any
+    two elements a and b to form another element, denoted a • b or ab. To qualify as a group, the
+    set and operation, (G, •), must satisfy four requirements known as the group axioms:
+
+    Closure
+       For all a, b in G, the result of the operation, a • b, is also in G.
+
+    Associativity
+        For all a, b and c in G, (a • b) • c = a • (b • c).
+
+    Identity element
+        There exists an element e in G such that, for every element a in G, the equation e • a = a •
+        e = a holds. Such an element is unique.
+
+    Inverse element
+        For each a in G, there exists an element b in G, commonly denoted a−1 (or −a, if the
+        operation is denoted "+"), such that a • b = b • a = e, where e is the identity element.
+
+    Subgroup
+
+    H is a subgroup of G if the restriction of • to H × H is a group operation on H. This is usually
+    denoted H ≤ G, read as "H is a subgroup of G".
+
+    """
+
+    ##############################################
+
+    def __init__(self, modulo):
+
+        self._modulo = modulo
+
+        self._table = [[self.operation(i, j) for i in range(modulo)] for j in range(modulo)]
+
+    ##############################################
+
+    @property
+    def modulo(self):
+        return self._modulo
+
+    @property
+    def cayley_table(self):
+        return self._table
+
+    ##############################################
+
+    def operation(self, a, b):
+
+        return (a + b) % self._modulo
+
+####################################################################################################
+
+class ET12GroupSingleton(AdditionModuloGroup):
+
+    ##############################################
+
+    def __init__(self):
+
+        super().__init__(modulo=12)
+
+        self._subgroups = (
+            # Fixme: unknown proof or algorithm to find them
+
+            # Pk = { i*k } for i,k and i*k in G
+            # 12 = 3 * 4 = 3 * 2 * 2 = 6 * 2
+
+            # k = 0 is C / Do
+            (0),
+            # k = 6 is tritone (C, F#) / (Do, Fa#)
+            (0, 6),
+            # k = 4 is augmented fifth chord (C, E, G#) / (Do, Mi, Sol#)
+            # triangle
+            (0, 4, 8),
+            # k = 3 is diminished seventh chord (C, Eb, F#, A) / (Do, Mib, Fa#, La)
+            # square
+            (0, 3, 6, 9),
+            # k = 2 is tone scale (C, D, E, F#, G#, Bb) / (Do, Ré, Mi, Fa#, Sol#, Sib)
+            # hexagon
+            (0, 2, 4, 6, 8, 10),
+            # k = 1 is G
+            # decagon
+        )
+
+    ##############################################
+
+    @property
+    def subgroups(self):
+        return self._subgroups
+
+####################################################################################################
+
+ET12Group = ET12GroupSingleton()
 
 ####################################################################################################
 
@@ -362,16 +465,21 @@ class EqualTemperamentPitch:
 
     ##############################################
 
-    def __init__(self, step, number_of_steps):
+    def __init__(self, step_number, number_of_steps):
 
-        self._step = step
+        self._step_number = step_number
         self._number_of_steps = number_of_steps
 
     ##############################################
 
+    def __repr__(self):
+        return "{0.__class__.__name__} {0._step_number} / {0._number_of_steps}".format(self)
+
+    ##############################################
+
     @property
-    def step(self):
-        return self._step
+    def step_number(self):
+        return self._step_number
 
     @property
     def number_of_steps(self):
@@ -384,22 +492,46 @@ class EqualTemperamentPitch:
     ##############################################
 
     def __float__(self):
-        return 2**(self._step / self._number_of_steps)
+        return 2**(self._step_number / self._number_of_steps)
 
     ##############################################
 
     def __lt__(self, other):
-        return float(self) < float(other)
+        # return float(self) < float(other)
+        return self._step_number < other.step_number
 
 ####################################################################################################
 
-class EqualTemperamentTuningSingleton:
+class EqualTemperamentTuning:
 
     ##############################################
 
-    def __init__(self, number_of_steps):
+    @staticmethod
+    def fifth_approximations(number_of_steps_max=20):
+
+        """Compute the best perfect fifth approximations having the form :math:`2^{i/j}`.
+
+        12-TET is based on :math:`2^{7/12} \approx 1.498` versus 1.5 for the perfect fifth.
+        """
+
+        approximations = []
+        for number_of_steps in range(1, number_of_steps_max +1):
+            for step_number in range(1, number_of_steps_max +1):
+                pitch = EqualTemperamentPitch(step_number, number_of_steps)
+                delta = abs(float(pitch) - FrequencyRatio.fifth)
+                if delta < .01:
+                    approximations.append(pitch)
+
+        return approximations
+
+    ##############################################
+
+    def __init__(self, number_of_steps, fifth_step_number):
 
         # Waring: include octave !
+
+        self._number_of_steps = number_of_steps
+        self._fifth_step_number = fifth_step_number
 
         self._pitchs = [EqualTemperamentPitch(step, number_of_steps)
                         for step in range(number_of_steps + 1)]
@@ -412,11 +544,104 @@ class EqualTemperamentTuningSingleton:
     ##############################################
 
     def __len__(self):
-        return len(self._pitchs)
+        return self._number_of_steps # len(self._pitchs)
 
     ##############################################
 
     def __getitem__(self, i):
         return self._pitchs[i]
 
-ET12Tuning = EqualTemperamentTuningSingleton(number_of_steps=12)
+    ##############################################
+
+    @property
+    def group(self):
+        return ET12Group
+
+    ##############################################
+
+    @property
+    def first_pitch(self):
+        return self._pitchs[0]
+
+    ##############################################
+
+    @property
+    def fourth(self):
+
+        """Return the fourth (F / Fa) which is the inversion of the fifth to the octave (5 = 12 - 7).
+
+        """
+
+        fourth_step_number = self._number_of_steps - self._fifth_step_number
+
+        return self._pitchs[fourth_step_number]
+
+    ##############################################
+
+    def _fifth_series(self, number_of_iterations):
+
+        fifth_series = [(i*self._fifth_step_number) % self._number_of_steps
+                        for i in range(1, number_of_iterations +1)]
+
+        return [self._pitchs[i] for i in fifth_series]
+
+    ##############################################
+
+    @property
+    def fifth_series(self):
+
+        """Return the complete fifth series up to the 7th octave.
+
+        (7,   2,  9,  4,  11, 6,   1,   8,    3,   10,  5,  0)
+        (G,   D,  A,  E,  B   F#,  C#,  G#,   D#,  Bb,  F,  C)
+        (Sol, Ré, La, Mi, Si, Fa#, Do#, Sol#, Ré#, Sib, Fa, Do)
+
+        """
+
+        return self._fifth_series(self._number_of_steps)
+
+    ##############################################
+
+    @property
+    def fifth_series_for_major_scale(self):
+
+        """Return the fifth series that constitute the major scale.
+
+        (7,   2,  9,  4,  11)
+        (G,   D,  A,  E,  B )
+        (Sol, Ré, La, Mi, Si)
+
+        and sorted
+
+        (2,  4,  7,   9,  11)
+        (D,  E,  G,   A,  B)
+        (Ré, Mi, Sol, La, Si)
+
+        Note the 6th fifth 6 / F#, is substituted by the fourth 5 / F since the major scale is made
+        of 7 notes, the first note, the fifth, the fourth, and 4 fifths to complete the set.
+
+        """
+
+        return self._fifth_series(self._fifth_step_number -2) # 7 - 1 - 1
+
+    ##############################################
+
+    @property
+    def major_scale(self):
+
+        """Return the major scale.
+
+        The major scale is made of 7 notes, the first note, the fifth, the fourth, and 4 fifths to
+        complete the set.
+
+        (0,  2,  4,  5,  7,   9,  11)
+        (C,  D,  E,  F,  G,   A,  B)
+        (Do, Ré, Mi, Fa, Sol, La, Si)
+
+        """
+
+        return sorted(self.fifth_series_for_major_scale + [self.first_pitch, self.fourth])
+
+####################################################################################################
+
+ET12Tuning = EqualTemperamentTuning(number_of_steps=12, fifth_step_number=7)
