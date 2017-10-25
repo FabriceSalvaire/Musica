cb0dc9c8f000c4a28455ef4ed7152a3423bbfa81
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-10-25 18:36:37 +0200

    Improved Quality.py

--- Musica/Theory/Quality.py
+++ Musica/Theory/Quality.py
@@ -30,36 +30,151 @@ __all__ = [
 
 ####################################################################################################
 
-class IntervalQualities:
+from Musica.Locale import translate
 
-    __quality_to_short__ = {
-        'perfect':    'P',
-        'minor':      'm',
-        'major':      'M',
-        'augmented':  'A',
-        'diminished': 'd',
-    }
+####################################################################################################
 
-    __short_to_quality__ = {value:key for key, value in __quality_to_short__.items()}
+class IntervalQuality:
 
-    __multiple__ =  (
-        None,        # 0
-        '',          # 1
-        'doubly',    # 2
-        'triply',    # 3
-        'quadruply', # 4
-        )
+    ##############################################
+
+    def __init__(self, name, short):
+
+        self._name = name
+        self._short = short
+
+    ##############################################
+
+    def __repr__(self):
+        return '{0.__class__.__name__} {0._name}'.format(self)
+
+    ##############################################
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def short(self):
+        return self._short
+
+    ##############################################
+
+    def locale_name(self, language):
+        return translate(self._name, language)
+
+    ##############################################
+
+    def format(self, degree):
+        return '{0._short}{1}'.format(self, degree)
+
+####################################################################################################
+
+class Multiple:
+
+    ##############################################
+
+    def __init__(self, name, value):
+
+        self._name = name
+        self._value = value
+
+    ##############################################
+
+    def __repr__(self):
+        return '{0.__class__.__name__} {0._name}'.format(self)
+
+    ##############################################
+
+    @property
+    def name(self):
+        return self._name
+
+    # @property
+    # def value(self):
+    #     return self._value
+
+    def __int__(self):
+        return self._value
+
+    ##############################################
+
+    def locale_name(self, language):
+        return translate(self._name, language)
+
+    ##############################################
+
+    def __lt__(self, other):
+        return self._value < int(other)
+
+####################################################################################################
+
+class IntervalQualitiesSingleton:
+
+    __qualities__ = (
+        IntervalQuality('perfect',    'P'),
+        IntervalQuality('minor',      'm'),
+        IntervalQuality('major',      'M'),
+        IntervalQuality('augmented',  'A'),
+        IntervalQuality('diminished', 'd'),
+    )
+
+    __quality_to_short__ = {quality.name:quality for quality in __qualities__}
+    __short_to_quality__ = {quality.short:quality for quality in __qualities__}
+
+    __map__ = dict(__quality_to_short__)
+    __map__.update(__short_to_quality__)
 
     ##############################################
 
     @classmethod
-    def short(cls, quality):
+    def __getitem__(cls, i):
+        return cls.__map__[i]
+
+    @classmethod
+    def from_name(cls, quality):
         return cls.__quality_to_short__[quality]
 
     @classmethod
-    def quality(cls, short):
+    def from_short(cls, short):
         return cls.__short_to_quality__[short]
 
+####################################################################################################
+
+IntervalQualities = IntervalQualitiesSingleton()
+
+####################################################################################################
+
+class MultiplesSingleton:
+
+    __multiples__ = [None] + [Multiple(name, i)
+                              for i, name in enumerate((
+                                      '',          # 1
+                                      'doubly',    # 2
+                                      'triply',    # 3
+                                      'quadruply', # 4
+                              ))]
+    __multiple_map__ = {multiple.name:multiple
+                        for multiple in __multiples__ if multiple is not None}
+
+    __map__ = {int(multiple):multiple for multiple in __multiple_map__.values()}
+    __map__.update(__multiple_map__)
+    __map__[0] = None
+
+    ##############################################
+
     @classmethod
-    def multiple(cls, i):
-        return cls.__multiple__[i]
+    def __getitem__(cls, i):
+        return cls.__map__[i]
+
+    @classmethod
+    def from_value(cls, value):
+        return cls.__multiples__[value]
+
+    @classmethod
+    def from_name(cls, name):
+        return cls.__multiple_map__[name]
+
+####################################################################################################
+
+Multiples = MultiplesSingleton()
